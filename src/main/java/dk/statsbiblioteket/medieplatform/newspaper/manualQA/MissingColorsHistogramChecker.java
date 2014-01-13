6d3218b9de39b26643bf04c9b1116bdde1ae15c1
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2014-01-13 11:37:58 +0100

    Missing Colors check

---
+++ src/main/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/MissingColorsHistogramChecker.java
@@ -0,0 +1,80 @@
+package dk.statsbiblioteket.medieplatform.newspaper.manualQA;
+
+import dk.statsbiblioteket.medieplatform.autonomous.ResultCollector;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.AttributeParsingEvent;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.DefaultTreeEventHandler;
+import dk.statsbiblioteket.medieplatform.newspaper.manualQA.flagging.FlaggingCollector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hvis vi finder den lyseste og den mørkeste farve på billedet, har vi et krav om at ingen farver mellem de to må være
+ * 0 eller meget lave. Det er ikke muligt for et analogt system at producere dette, så det er et klart tegn på
+ * efterbehandling.
+ */
+public class MissingColorsHistogramChecker extends DefaultTreeEventHandler {
+
+    private dk.statsbiblioteket.medieplatform.newspaper.manualQA.flagging.FlaggingCollector flaggingCollector;
+    private ResultCollector resultCollector;
+    private int threshold;
+
+    public MissingColorsHistogramChecker(ResultCollector resultCollector, FlaggingCollector flaggingCollector,
+                                         int threshold) {
+        this.flaggingCollector = flaggingCollector;
+        this.resultCollector = resultCollector;
+        this.threshold = threshold;
+    }
+
+    @Override
+    public void handleAttribute(AttributeParsingEvent event) {
+        try {
+            if (event.getName().endsWith(".histogram.xml")) {
+                Histogram histogram = new Histogram(event.getData());
+                List<Integer> missingColorList = findMissingColors(histogram);
+                if (missingColorList.size() > threshold) {
+                    flaggingCollector.addFlag(
+                            event,
+                            "jp2file",
+                            getComponent(),
+                            "There are '" + missingColorList.size() + " in the picture. This could be the result of post processing");
+                }
+            }
+        } catch (Exception e) {
+            resultCollector.addFailure(event.getName(), "Exception", "component", e.getMessage());
+        }
+
+    }
+
+    private String getComponent() {
+        return null;
+    }
+
+    private List<Integer> findMissingColors(Histogram histogram) {
+        long[] values = histogram.values();
+        List<Integer> result = new ArrayList<>();
+
+        int lowest = -1;
+        int highest = -1;
+        for (int i = 0; i < values.length; i++) {
+            long value = values[i];
+            if (lowest == -1 && value > 0) {
+                lowest = i;
+            }
+            if (value > 0) {
+                highest = i;
+            }
+
+        }
+        for (int i = lowest; i <= highest; i++) {
+            long value = values[i];
+
+            if (value == 0) {
+                result.add(i);
+
+            }
+        }
+        return result;
+
+    }
+}
---
+++ src/test/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/MissingColorsHistogramCheckerTest.java
@@ -0,0 +1,45 @@
+package dk.statsbiblioteket.medieplatform.newspaper.manualQA;
+
+import dk.statsbiblioteket.medieplatform.autonomous.Batch;
+import dk.statsbiblioteket.medieplatform.autonomous.ResultCollector;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.AttributeParsingEvent;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.TreeEventHandler;
+import dk.statsbiblioteket.medieplatform.newspaper.manualQA.flagging.FlaggingCollector;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MissingColorsHistogramCheckerTest {
+    @Test
+    public void testHandleAttributeGood() throws Exception {
+        ResultCollector resultCollector = new ResultCollector("blah", "blah");
+        FlaggingCollector flaggingCollector = new FlaggingCollector(new Batch("40000"), null, "0.1-SNAPSHOT");
+        //Threshold 0 so this expects perfect linearity
+        TreeEventHandler histogramHandler = new MissingColorsHistogramChecker(
+                resultCollector, flaggingCollector, 0);
+        AttributeParsingEvent event = createAttributeEvent(
+                "B400022028252-RT1/400022028252-08/1795-12-20-01/adresseavisen1759-1795-12-20-01-0079.jp2.histogram.xml",
+                HistogramXml.getSampleGoodHistogram());
+        histogramHandler.handleAttribute(event);
+        Assert.assertFalse(flaggingCollector.hasFlags(), flaggingCollector.toReport());
+    }
+
+    private AttributeParsingEvent createAttributeEvent(String name, final String contents) {
+        return new AttributeParsingEvent(name) {
+            @Override
+            public InputStream getData() throws IOException {
+                return new ByteArrayInputStream(contents.getBytes());
+
+            }
+
+            @Override
+            public String getChecksum() throws IOException {
+                return null;
+            }
+        };
+    }
+
+}
