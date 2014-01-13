bfafbc14ca96b87acc0c3fb5b8bc66d6d93422c5
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2014-01-13 10:48:56 +0100

    Choppy curve checker

---
+++ src/main/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/ChoppyCurveHistogramChecker.java
@@ -0,0 +1,71 @@
+package dk.statsbiblioteket.medieplatform.newspaper.manualQA;
+
+import dk.statsbiblioteket.medieplatform.autonomous.ResultCollector;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.AttributeParsingEvent;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.DefaultTreeEventHandler;
+import dk.statsbiblioteket.medieplatform.newspaper.manualQA.flagging.FlaggingCollector;
+
+/**
+ * Hakker i kurven
+ * Vi så en del kurver der ikke manglede farver, men havde vilde variationer mellem to farver. En test er at rulle et
+ * window af bredde 3 hen over kurven. For hver punkt skal det gælde at (x0+x2)/2 ~= x1. Undervejs tæller vi så op hvor
+ * meget off den ligning er, og hvis den slutteligt er over en hvis størrelse, kan vi flagge billedet.
+ */
+public class ChoppyCurveHistogramChecker extends DefaultTreeEventHandler {
+
+    private final ResultCollector resultCollector;
+    private final FlaggingCollector flaggingCollector;
+    private final long threshold;
+
+    public ChoppyCurveHistogramChecker(ResultCollector resultCollector, FlaggingCollector flaggingCollector, long threshold) {
+        this.resultCollector = resultCollector;
+        this.flaggingCollector = flaggingCollector;
+        this.threshold = threshold;
+    }
+
+
+    @Override
+    public void handleAttribute(AttributeParsingEvent event) {
+        try {
+            if (event.getName().endsWith(".histogram.xml")) {
+                Histogram histogram = new Histogram(event.getData());
+                long error = testChoppyness(histogram);
+                if (error > threshold) {
+                    flaggingCollector.addFlag(
+                            event,
+                            "jp2file",
+                            getComponent(),
+                            "Unnaturally choppy histogram curve. Possible sign of post processing");
+                }
+            }
+        } catch (Exception e) {
+            resultCollector.addFailure(event.getName(), "Exception", "component", e.getMessage());
+        }
+
+    }
+
+    private String getComponent() {
+        return getClass().getName();
+    }
+
+    private long testChoppyness(Histogram histogram) {
+
+        long error = 0;
+        long[] window = new long[3];
+        long[] values = histogram.values();
+        for (int i = 0; i < values.length; i++) {
+            long value = values[i];
+            window[0] = window[1];
+            window[1] = window[2];
+            window[2] = value;
+            if (i < 2) { //we have not filled the first window yet
+                continue;
+            }
+            long expected = (window[0] + window[2]) / 2;
+            long increase = Math.abs(expected - window[1]);
+            error += increase;
+            System.out.println(i-1+" "+increase);
+        }
+        return error;
+    }
+}
---
+++ src/test/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/ChoppyCurveHistogramCheckerTest.java
@@ -0,0 +1,44 @@
+package dk.statsbiblioteket.medieplatform.newspaper.manualQA;
+
+import dk.statsbiblioteket.medieplatform.autonomous.Batch;
+import dk.statsbiblioteket.medieplatform.autonomous.ResultCollector;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.AttributeParsingEvent;
+import dk.statsbiblioteket.medieplatform.newspaper.manualQA.flagging.FlaggingCollector;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ChoppyCurveHistogramCheckerTest {
+    @Test
+    public void testHandleAttributeBad() throws Exception {
+        ResultCollector resultCollector = new ResultCollector("blah", "blah");
+        FlaggingCollector flaggingCollector = new FlaggingCollector(new Batch("40000"), null,"0.1-SNAPSHOT");
+        //Threshold 0 so this expects perfect linearity
+        ChoppyCurveHistogramChecker histogramHandler = new ChoppyCurveHistogramChecker(
+                resultCollector, flaggingCollector,0);
+        AttributeParsingEvent event = createAttributeEvent(
+                "B400022028252-RT1/400022028252-08/1795-12-20-01/adresseavisen1759-1795-12-20-01-0079.jp2.histogram.xml",
+                HistogramXml.getSampleGoodHistogram());
+        histogramHandler.handleAttribute(event);
+        Assert.assertFalse(flaggingCollector.hasFlags(), flaggingCollector.toReport());
+    }
+
+    private AttributeParsingEvent createAttributeEvent(String name, final String contents) {
+        return new AttributeParsingEvent(name) {
+            @Override
+            public InputStream getData() throws IOException {
+                return new ByteArrayInputStream(contents.getBytes());
+
+            }
+
+            @Override
+            public String getChecksum() throws IOException {
+                return null;
+            }
+        };
+    }
+
+}
