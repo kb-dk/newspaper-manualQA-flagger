28efb2b767019d1f8774aa7bcf8797157f3091f7
jrgatsb <jrg@statsbiblioteket.dk>
2014-01-16 12:53:17 +0100

    Unit test for new algorithm for finding choppiness

--- src/test/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/ChoppyCurveHistogramCheckerTest.java
+++ src/test/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/ChoppyCurveHistogramCheckerTest.java
@@ -13,25 +13,47 @@ import java.io.InputStream;
 
 public class ChoppyCurveHistogramCheckerTest {
     @Test
-    public void testHandleAttributeBad() throws Exception {
+    public void testHandleAttributeGood() throws Exception {
         ResultCollector resultCollector = new ResultCollector("blah", "blah");
         FlaggingCollector flaggingCollector = new FlaggingCollector(new Batch("40000"), null, "0.1-SNAPSHOT");
-        //
+
         ChoppyCurveHistogramChecker histogramHandler = new ChoppyCurveHistogramChecker(
-                resultCollector, flaggingCollector, 0.5, 4);
+                resultCollector, flaggingCollector, 0.1, 4);
         AttributeParsingEvent event = createAttributeEvent(
                 "B400022028252-RT1/400022028252-08/1795-12-20-01/adresseavisen1759-1795-12-20-01-0079.jp2.histogram.xml",
                 HistogramXml.getSampleGoodHistogram());
         histogramHandler.handleAttribute(event);
-        Assert.assertTrue(flaggingCollector.hasFlags(), flaggingCollector.toReport());
+
+        Assert.assertFalse(flaggingCollector.hasFlags(), flaggingCollector.toReport());
+
+        System.out.println(flaggingCollector.toReport());
     }
+
+
+    @Test
+    public void testHandleAttributeBad() throws Exception {
+        ResultCollector resultCollector = new ResultCollector("blah", "blah");
+        FlaggingCollector flaggingCollector = new FlaggingCollector(new Batch("40000"), null, "0.1-SNAPSHOT");
+
+        ChoppyCurveHistogramChecker histogramHandler = new ChoppyCurveHistogramChecker(
+                resultCollector, flaggingCollector, 0.1, 4);
+        AttributeParsingEvent event = createAttributeEvent(
+                "B400022028252-RT1/400022028252-08/1795-12-20-01/adresseavisen1759-1795-12-20-01-0079.jp2.histogram.xml",
+                HistogramXml.getSampleBadHistogram());
+        histogramHandler.handleAttribute(event);
+
+        Assert.assertTrue(flaggingCollector.hasFlags(), flaggingCollector.toReport());
+
+        System.out.println(flaggingCollector.toReport());
+    }
+
+
 
     private AttributeParsingEvent createAttributeEvent(String name, final String contents) {
         return new AttributeParsingEvent(name) {
             @Override
             public InputStream getData() throws IOException {
                 return new ByteArrayInputStream(contents.getBytes());
-
             }
 
             @Override
