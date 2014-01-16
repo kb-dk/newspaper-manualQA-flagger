2fc9f37395a2e854f410b5adcf3116f922db8c03
jrgatsb <jrg@statsbiblioteket.dk>
2014-01-16 11:12:23 +0100

    Histogram with "post-processing artifacts" incorporated - for unit tests

--- src/test/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/HistogramXml.java
+++ src/test/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/HistogramXml.java
@@ -7,6 +7,15 @@ import java.io.IOException;
 public class HistogramXml {
 
     public static String getSampleGoodHistogram() throws IOException {
-        return Strings.flush(Thread.currentThread().getContextClassLoader().getResourceAsStream("histogramExample.xml"));
+        return Strings.flush(Thread.currentThread().getContextClassLoader().getResourceAsStream(
+                "histogramExample.xml"));
     }
+
+    public static String getSampleBadHistogram() throws IOException {
+        // The "BadBadHistogram.xml" is an actual histogram from 9* with possible post-processing artifacts
+        return Strings.flush(Thread.currentThread().getContextClassLoader().getResourceAsStream(
+                "BadBadHistogram.xml"));
+//                "histogramExampleBad.xml"));
+    }
+
 }
