6acdc5ff5120e31da2b60aab06d7da41547ef5b1
jrgatsb <jrg@statsbiblioteket.dk>
2014-01-17 12:01:54 +0100

    Unit test

--- src/test/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/MissingColorsHistogramCheckerTest.java
+++ src/test/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/MissingColorsHistogramCheckerTest.java
@@ -19,12 +19,26 @@ public class MissingColorsHistogramCheckerTest {
         FlaggingCollector flaggingCollector = new FlaggingCollector(new Batch("40000"), null, "0.1-SNAPSHOT");
         //Threshold 0 so this expects perfect linearity
         TreeEventHandler histogramHandler = new MissingColorsHistogramChecker(
-                resultCollector, flaggingCollector, 0);
+                resultCollector, flaggingCollector, 0, 10);
         AttributeParsingEvent event = createAttributeEvent(
                 "B400022028252-RT1/400022028252-08/1795-12-20-01/adresseavisen1759-1795-12-20-01-0079.jp2.histogram.xml",
                 HistogramXml.getSampleGoodHistogram());
         histogramHandler.handleAttribute(event);
         Assert.assertFalse(flaggingCollector.hasFlags(), flaggingCollector.toReport());
+    }
+
+    @Test
+    public void testHandleAttributeBad() throws Exception {
+        ResultCollector resultCollector = new ResultCollector("blah", "blah");
+        FlaggingCollector flaggingCollector = new FlaggingCollector(new Batch("40000"), null, "0.1-SNAPSHOT");
+        //Threshold 0 so this expects perfect linearity
+        TreeEventHandler histogramHandler = new MissingColorsHistogramChecker(
+                resultCollector, flaggingCollector, 0, 10);
+        AttributeParsingEvent event = createAttributeEvent(
+                "B400022028252-RT1/400022028252-08/1795-12-20-01/adresseavisen1759-1795-12-20-01-0079.jp2.histogram.xml",
+                HistogramXml.getSampleBadHistogram());
+        histogramHandler.handleAttribute(event);
+        Assert.assertTrue(flaggingCollector.hasFlags(), flaggingCollector.toReport());
     }
 
     private AttributeParsingEvent createAttributeEvent(String name, final String contents) {
