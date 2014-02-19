4d54a5ff732058e8abf37628eb595e1a65abba82
jrgatsb <jrg@statsbiblioteket.dk>
2014-02-19 13:12:00 +0100

    Fixed constants

--- src/test/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/HistogramXml.java
+++ src/test/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/HistogramXml.java
@@ -5,27 +5,35 @@ import dk.statsbiblioteket.util.Strings;
 import java.io.IOException;
 
 public class HistogramXml {
+    private static final String GOOD_HIST = "histogramExample.xml";
+
+    // The "BadBadHistogram.xml" is an actual histogram from 9* with possible post-processing artifacts
+    // Formerly "histogramExampleBad.xml"
+    private static final String BAD_BAD_HIST = "BadBadHistogram.xml";
+
+    private static final String BAD_DARKNESS_HIST = "histogramExampleBadDarkness.xml";
+    private static final String ALTO_HI = "altoExampleHiTextAmount.xml";
+    private static final String ALTO_LO = "altoExampleLoTextAmount.xml";
+
 
     public static String getSampleGoodHistogram() throws IOException {
-        return getXml("histogramExample.xml");
+        return getXml(GOOD_HIST);
     }
 
     public static String getSampleBadHistogram() throws IOException {
-        // The "BadBadHistogram.xml" is an actual histogram from 9* with possible post-processing artifacts
-        return getXml("BadBadHistogram.xml");
-//                "histogramExampleBad.xml"
+        return getXml(BAD_BAD_HIST);
     }
 
     public static String getSampleBadDarknessHistogram() throws IOException {
-        return getXml("histogramExampleBadDarkness.xml");
+        return getXml(BAD_DARKNESS_HIST);
     }
 
     public static String getSampleAltoHiText() throws IOException {
-        return getXml("altoExampleHiTextAmount.xml");
+        return getXml(ALTO_HI);
     }
 
     public static String getSampleAltoLoText() throws IOException {
-        return getXml("altoExampleLoTextAmount.xml");
+        return getXml(ALTO_LO);
     }
 
     private static String getXml(String file) throws IOException {
