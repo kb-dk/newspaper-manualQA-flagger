f448d403755ee331c14ef1fb681a74776e07ba94
jrgatsb <jrg@statsbiblioteket.dk>
2014-03-04 13:30:04 +0100

    Making configuration of edition mods handler configurable

--- src/main/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/FlaggerFactory.java
+++ src/main/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/FlaggerFactory.java
@@ -11,11 +11,30 @@ import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.TreeE
 import dk.statsbiblioteket.medieplatform.newspaper.manualQA.flagging.FlaggingCollector;
 
 public class FlaggerFactory implements EventHandlerFactory {
+    // MissingColorsHistogramChecker:
+    private static final int MISSING_COLORS_NUMBER_OF_MISSING_ALLOWED = 0;
+    private static final int MISSING_COLORS_MAX_VAL_TO_DEEM_A_COLOR_MISSING = 10;
+
+    // ChoppyCurveHistogramChecker:
     // How much a value is allowed to deviate from the average of its two neighbours (in pct, >0) before
     // it is considered an irregularity (only happens if both neighbours are either higher or lower)
     private static final double CHOPPY_CHECK_THRESHOLD = 0.5;
     // The maximum number of peaks/valleys allowed before flagged as an error
     private static final int CHOPPY_CHECK_MAX_IRREGULARITIES = 4;
+
+    // EditionModsHandler:
+    // The maximum number of editions of a newspaper per day before we raise a flag
+    private static final int EDITION_MODS_MAX_EDITIONS_PER_DAY = 3;
+
+
+    // EndSpikeHistogramChecker:
+    private static final double END_SPIKE_THRESHOLD = 0.1;
+    private static final int END_SPIKE_MIN_COLOR_CONSIDERED_BLACK = 0;
+    private static final int END_SPIKE_MAX_COLOR_CONSIDERED_BLACK = 2;
+    private static final int END_SPIKE_MIN_COLOR_CONSIDERED_WHITE = 255;
+    private static final int END_SPIKE_MAX_COLOR_CONSIDERED_WHITE = 255;
+    private static final double END_SPIKE_MAX_PERCENT_ALLOWED_NEAR_BLACK = 2;
+    private static final double END_SPIKE_MAX_PERCENT_ALLOWED_NEAR_WHITE = 0.5;
 
     private final ResultCollector resultCollector;
     private final Batch batch;
@@ -34,14 +53,20 @@ public class FlaggerFactory implements EventHandlerFactory {
     @Override
     public List<TreeEventHandler> createEventHandlers() {
         ArrayList<TreeEventHandler> treeEventHandlers = new ArrayList<>();
-        treeEventHandlers.add(new MissingColorsHistogramChecker(resultCollector, flaggingCollector, 0, 10));
-        treeEventHandlers.add(new ChoppyCurveHistogramChecker(resultCollector, flaggingCollector,
-                CHOPPY_CHECK_THRESHOLD, CHOPPY_CHECK_MAX_IRREGULARITIES));
-        treeEventHandlers.add(new EditionModsHandler(resultCollector, flaggingCollector, batch));
+        treeEventHandlers.add(new MissingColorsHistogramChecker(resultCollector, flaggingCollector,
+                MISSING_COLORS_NUMBER_OF_MISSING_ALLOWED, MISSING_COLORS_MAX_VAL_TO_DEEM_A_COLOR_MISSING));
+        treeEventHandlers.add(new ChoppyCurveHistogramChecker(resultCollector, flaggingCollector, CHOPPY_CHECK_THRESHOLD,
+                CHOPPY_CHECK_MAX_IRREGULARITIES));
+        treeEventHandlers.add(new EditionModsHandler(resultCollector, flaggingCollector, batch,
+                EDITION_MODS_MAX_EDITIONS_PER_DAY));
         treeEventHandlers.add(new FilmHandler(resultCollector, flaggingCollector));
         treeEventHandlers.add(new MixHandler(resultCollector, properties, flaggingCollector));
         treeEventHandlers.add(new AltoWordAccuracyChecker(resultCollector, flaggingCollector, properties));
         treeEventHandlers.add(new DarknessHistogramChecker(resultCollector, flaggingCollector, batch, 300, 3, 128, 50));
+        treeEventHandlers.add(new EndSpikeHistogramChecker(resultCollector, flaggingCollector, END_SPIKE_THRESHOLD,
+                END_SPIKE_MIN_COLOR_CONSIDERED_BLACK, END_SPIKE_MAX_COLOR_CONSIDERED_BLACK, END_SPIKE_MIN_COLOR_CONSIDERED_WHITE,
+                END_SPIKE_MAX_COLOR_CONSIDERED_WHITE, END_SPIKE_MAX_PERCENT_ALLOWED_NEAR_BLACK,
+                END_SPIKE_MAX_PERCENT_ALLOWED_NEAR_WHITE));
         return treeEventHandlers;
     }
 }
