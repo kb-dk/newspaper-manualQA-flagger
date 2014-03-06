3b6543bb7eeb3fbc261a296fa27c3b362f1806ba
jrgatsb <jrg@statsbiblioteket.dk>
2014-03-06 14:21:00 +0100

    Class for excluding the UNMATCHED folder from flagging checks

---
+++ src/main/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/UnmatchedExcluder.java
@@ -0,0 +1,44 @@
+package dk.statsbiblioteket.medieplatform.newspaper.manualQA;
+
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.AttributeParsingEvent;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.NodeBeginsParsingEvent;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.NodeEndParsingEvent;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.DefaultTreeEventHandler;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.TreeEventHandler;
+
+public class UnmatchedExcluder extends DefaultTreeEventHandler {
+    private TreeEventHandler delegate;
+
+    public UnmatchedExcluder(TreeEventHandler delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void handleNodeBegin(NodeBeginsParsingEvent event) {
+        if (event.getName().contains("UNMATCHED")) {
+            return;
+        }
+        delegate.handleNodeBegin(event);
+    }
+
+    @Override
+    public void handleNodeEnd(NodeEndParsingEvent event) {
+        if (event.getName().contains("UNMATCHED")) {
+            return;
+        }
+        delegate.handleNodeEnd(event);
+    }
+
+    @Override
+    public void handleAttribute(AttributeParsingEvent event) {
+        if (event.getName().contains("UNMATCHED")) {
+            return;
+        }
+        delegate.handleAttribute(event);
+    }
+
+    @Override
+    public void handleFinish() {
+        delegate.handleFinish();
+    }
+}
