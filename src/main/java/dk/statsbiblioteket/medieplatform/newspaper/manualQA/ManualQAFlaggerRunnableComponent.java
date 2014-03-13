5401a84819d4a62ec77dab6372f7ce65922878ad
jrgatsb <jrg@statsbiblioteket.dk>
2014-03-13 13:50:43 +0100

    Fixed redundant import

--- src/main/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/ManualQAFlaggerRunnableComponent.java
+++ src/main/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/ManualQAFlaggerRunnableComponent.java
@@ -15,7 +15,6 @@ import dk.statsbiblioteket.doms.central.connectors.EnhancedFedora;
 import dk.statsbiblioteket.doms.central.connectors.EnhancedFedoraImpl;
 import dk.statsbiblioteket.doms.central.connectors.fedora.pidGenerator.PIDGeneratorException;
 import dk.statsbiblioteket.doms.webservices.authentication.Credentials;
-import dk.statsbiblioteket.medieplatform.autonomous.AbstractRunnableComponent;
 import dk.statsbiblioteket.medieplatform.autonomous.Batch;
 import dk.statsbiblioteket.medieplatform.autonomous.ConfigConstants;
 import dk.statsbiblioteket.medieplatform.autonomous.ResultCollector;
