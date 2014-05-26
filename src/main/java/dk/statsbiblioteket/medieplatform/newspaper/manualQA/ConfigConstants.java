3a9a9a645d2dca1422e2c592949d3830d7aaaa30
jrg <jrg@statsbiblioteket.dk>
2014-05-26 16:24:52 +0200

    Minor cleanup

--- src/main/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/ConfigConstants.java
+++ src/main/java/dk/statsbiblioteket/medieplatform/newspaper/manualQA/ConfigConstants.java
@@ -1,16 +1,12 @@
 package dk.statsbiblioteket.medieplatform.newspaper.manualQA;
 
-
 public class ConfigConstants {
-
     private ConfigConstants(){}
 
     public static final String FLAG_IGNORE_COLORS_BELOW = "flag.ignoreColorsBelow";
     public static final String MAX_VAL_TO_DEEM_A_COLOR_MISSING = "flag.maxValueToDeemAColorMissing";
 
-
     public static final String NUMBER_OF_MISSING_COLORS_ALLOWED = "flag.missingcolors.numberOfMissingColorsAllowed";
-
 
     public static final String CHOPPY_CHECK_MAX_IRREGULARITIES = "flag.choppyCurve.checkMaxIrregularities";
     public static final String CHOPPY_CHECK_THRESHOLD = "flag.choppyCurve.checkthreshold";
@@ -22,17 +18,12 @@ public class ConfigConstants {
     public static final String DARKNESS_MIN_NUM_OF_TEXT_LINES = "flag.darkness.minNumOfTextLines";
     public static final String DARKNESS_HISTOGRAM_CHECKER_ON = "flag.darkness.histogramCheckerOn";
 
-
-
     public static final String END_SPIKE_THRESHOLD = "flag.endspike.threshold";
     public static final String END_SPIKE_MIN_COLOR_CONSIDERED_BLACK = "flag.endspike.minColorConsideredBlack";
     public static final String END_SPIKE_MIN_COLOR_CONSIDERED_WHITE = "flag.endspike.minColorConsideredWhite";
     public static final String END_SPIKE_MAX_COLOR_CONSIDERED_WHITE = "flag.endspike.maxColorConsideredWhite";
     public static final String END_SPIKE_MAX_PERCENT_ALLOWED_NEAR_BLACK = "flag.endspike.maxPercentAllowedNearBlack";
     public static final String END_SPIKE_MAX_PERCENT_ALLOWED_NEAR_WHITE = "flag.endspike.maxPercentAllowedNearWhite";
-
-
-
 
     public static final String SCANNER_MANUFACTURERS = "flag.mix.scannermanufacturers";
     public static final String SCANNER_MODELS = "flag.mix.scannermodels";
@@ -50,7 +41,6 @@ public class ConfigConstants {
     public static final String MINIMUM_ALTO_PERFILE_ACCURACY = "flag.alto.accuracy.perfile";
     public static final String ALTO_IGNORE_ZERO_ACCURACY = "flag.alto.zeroaccuracy.ignore";
     public static final String ALTO_WORD_ACCURACY_CHECKER_ON = "flag.alto.checker.on";
-
 
     public static final String MAX_FLAGS = "flag.maxtotalflags";
     
